8b96bce224c256d1cd4ba6f47dad430dc2cd9979
YTRodi <yagorodi@gmail.com>
2021-02-23 20:20:29 -0300

    add middleware in the add path

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,13 +4,31 @@
 const express = require('express');
 const router = express.Router();
 
+// Middlewares
+const { check } = require('express-validator');
+const { fieldsValidator } = require('../middlewares/fieldsValidator');
+
 // Controllers
 const { add, list, getPostById, update, deletePost } = require('../controllers/posts');
 
-router.post('/', add);
+router.post(
+	'/',
+	[
+		check('title', 'The title is requiered').not().isEmpty(),
+		check('content', 'The content is requiered').not().isEmpty(),
+		check('image', 'The image is requiered').not().isEmpty(),
+		check('category', 'The category is requiered').not().isEmpty(),
+		fieldsValidator,
+	],
+	add
+);
+
 router.get('/', list);
+
 router.get('/:id', getPostById);
+
 router.patch('/:id', update);
+
 router.delete('/:id', deletePost);
 
 module.exports = router;
