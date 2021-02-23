c37145f53886dc3838d2e25be4ea303109d96d59
YTRodi <yagorodi@gmail.com>
2021-02-23 15:47:47 -0300

    add Post model

---
+++ src/models/Post.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelizeInstance) => {
+	const Post = sequelizeInstance.define(
+		'Post',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			content: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			image: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			category: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			createdAt: {
+				type: DataTypes.DATE,
+				allowNull: false,
+			},
+		},
+		{
+			timestamps: false,
+		}
+	);
+
+	return Post;
+};
